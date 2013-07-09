f9bd5be7e7e385a69b65205708d7e422d67465a3
Brad Palmquist <brad.palmquist@gmail.com>
2013-07-09 11:17:45 +0000

    read index.html for response string

--- web.js
+++ web.js
@@ -1,9 +1,14 @@
 var express = require('express');
+var express = require('fs');
 
 var app = express.createServer(express.logger());
 
+// var respbuffer = fs.readFile('index.html');
+// var respstring = respbuffer.toString();
+var respstr = fs.readFile('index.html', 'utf8');
+
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  response.send(respstr);
 });
 
 var port = process.env.PORT || 5000;
